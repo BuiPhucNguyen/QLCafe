434da8d82a85001d776b9dc6d3daaaa725dfba7d
BuiPhucNguyen <pnguyen.93.012@gmail.com>
2022-10-30 23:58:27 +0700

    fix frame thanh toan

--- src/main/java/app/FrameThanhToan.java
+++ src/main/java/app/FrameThanhToan.java
@@ -35,14 +35,6 @@ import javax.swing.table.TableCellRenderer;
 
 import org.jdesktop.swingx.autocomplete.AutoCompleteDecorator;
 
-//import connectDB.ConnectDB;
-//import dao.DatPhong_DAO;
-//import dao.PhongHat_DAO;
-//import dao.TraPhong_DAO;
-//import entity.HoaDon;
-//import entity.HoaDonDichVu;
-//import entity.NhanVien;
-//import entity.PhongHat;
 
 public class FrameThanhToan extends JFrame  {
 
@@ -59,9 +51,6 @@ public class FrameThanhToan extends JFrame  {
 	private JButton btnThanhToan;
 	private JTextField txtThoiGianDen;
 	private JTextField txtThoiGianTra;
-//	private static PhongHat_DAO phongHat_dao;
-//	private TraPhong_DAO traPhong_dao;
-//	private static DatPhong_DAO datPhong_DAO;
 	private Date thoiGianTraPhongDate;
 	public static JComboBox<String> cmbChonPhong;
 	private JLabel lblTenKH;
@@ -72,16 +61,6 @@ public class FrameThanhToan extends JFrame  {
 
 	public JPanel createPanelTraPhong() {
 
-		// khởi tạo kết nối đến CSDL
-//		try {
-//			ConnectDB.getInstance().connect();
-//		} catch (SQLException e) {
-//			e.printStackTrace();
-//		}
-//		phongHat_dao = new PhongHat_DAO();
-//		traPhong_dao = new TraPhong_DAO();
-//		datPhong_DAO = new DatPhong_DAO();
-		// ------------------------------
 
 		setTitle("THANH TOÁN");
 		setSize(948, 440);
@@ -198,15 +177,6 @@ public class FrameThanhToan extends JFrame  {
 		txtNhanVien.setFont(new Font("Times New Roman", Font.BOLD, 13));
 		pnThongTinHoaDon.add(txtNhanVien);
 
-//		lblTenKH = new JLabel("TÊN KHÁCH HÀNG: ");
-//		lblTenKH.setBounds(15, 82, 150, 25);
-//		lblTenKH.setFont(new Font("Arial", Font.BOLD, 13));
-//		pnThongTinHoaDon.add(lblTenKH);
-//		txtKH = new JTextField();
-//		txtKH.setEditable(false);
-//		txtKH.setBounds(145, 80, 150, 30);
-//		txtKH.setFont(new Font("Times New Roman", Font.BOLD, 13)); 45 setBounds(15, 127, 150, 25); 
-//		pnThongTinHoaDon.add(txtKH);
 
 		JLabel lblTenPhong = new JLabel("TÊN BÀN: ");
 		lblTenPhong.setBounds(15, 82, 150, 25);
@@ -217,56 +187,6 @@ public class FrameThanhToan extends JFrame  {
 		txtTenPhong.setBounds(145, 80, 150, 30);
 		txtTenPhong.setFont(new Font("Times New Roman", Font.BOLD, 13));
 		pnThongTinHoaDon.add(txtTenPhong);
-
-//		JLabel lblLoaiPhong = new JLabel("LOẠI PHÒNG: ");
-//		lblLoaiPhong.setBounds(15, 172, 150, 25);
-//		lblLoaiPhong.setFont(new Font("Arial", Font.BOLD, 13));
-//		pnThongTinHoaDon.add(lblLoaiPhong);
-//		txtLoaiPhong = new JTextField();
-//		txtLoaiPhong.setEditable(false);
-//		txtLoaiPhong.setBounds(145, 170, 150, 30);
-//		txtLoaiPhong.setFont(new Font("Times New Roman", Font.BOLD, 13));
-//		pnThongTinHoaDon.add(txtLoaiPhong);
-
-//		JLabel lblGiaPhong = new JLabel("GIÁ PHÒNG/1H: ");
-//		lblGiaPhong.setBounds(15, 217, 150, 25);
-//		lblGiaPhong.setFont(new Font("Arial", Font.BOLD, 13));
-//		pnThongTinHoaDon.add(lblGiaPhong);
-//		txtGiaPhong = new JTextField();
-//		txtGiaPhong.setEditable(false);
-//		txtGiaPhong.setBounds(145, 215, 150, 30);
-//		txtGiaPhong.setFont(new Font("Times New Roman", Font.BOLD, 13));
-//		pnThongTinHoaDon.add(txtGiaPhong);
-
-//		JLabel lblThoiGianDen = new JLabel("THỜI GIAN ĐẾN: ");
-//		lblThoiGianDen.setBounds(15, 262, 150, 25);
-//		lblThoiGianDen.setFont(new Font("Arial", Font.BOLD, 13));
-//		pnThongTinHoaDon.add(lblThoiGianDen);
-//		txtThoiGianDen = new JTextField();
-//		txtThoiGianDen.setEditable(false);
-//		txtThoiGianDen.setFont(new Font("Times New Roman", Font.BOLD, 13));
-//		txtThoiGianDen.setBounds(145, 260, 150, 30);
-//		pnThongTinHoaDon.add(txtThoiGianDen);
-
-//		JLabel lblThoiGianTra = new JLabel("THỜI GIAN TRẢ: ");
-//		lblThoiGianTra.setBounds(15, 307, 150, 25);
-//		lblThoiGianTra.setFont(new Font("Arial", Font.BOLD, 13));
-//		pnThongTinHoaDon.add(lblThoiGianTra);
-//		txtThoiGianTra = new JTextField();
-//		txtThoiGianTra.setEditable(false);
-//		txtThoiGianTra.setFont(new Font("Times New Roman", Font.BOLD, 13));
-//		txtThoiGianTra.setBounds(145, 305, 150, 30);
-//		pnThongTinHoaDon.add(txtThoiGianTra);
-
-//		JLabel lblSoGio = new JLabel("TỔNG GIỜ THUÊ: ");
-//		lblSoGio.setBounds(15, 352, 150, 25);
-//		lblSoGio.setFont(new Font("Arial", Font.BOLD, 13));
-//		pnThongTinHoaDon.add(lblSoGio);
-//		txtSoGio = new JTextField();
-//		txtSoGio.setEditable(false);
-//		txtSoGio.setFont(new Font("Times New Roman", Font.BOLD, 13));
-//		txtSoGio.setBounds(145, 350, 150, 30);
-//		pnThongTinHoaDon.add(txtSoGio);
 
 		JLabel lblTienPhong = new JLabel("TIỀN DỊCH VỤ:");
 		lblTienPhong.setBounds(15, 127, 150, 25);
@@ -316,192 +236,10 @@ public class FrameThanhToan extends JFrame  {
 		btnThanhToan.setFocusPainted(false);
 		pnThongTinHoaDon.add(btnThanhToan);
 
-		/*
-		 * ADDACTION
-		 */
-//		btnThanhToan.addActionListener(this);
-//		btnInHoaDon.addActionListener(this);
-//		btnTimPhong.addActionListener(this);
-//		tableChonPhong.addMouseListener(this);
-//		cmbChonPhong.setSelectedIndex(-1);
-//		cmbChonPhong.addActionListener(this);
-//		docDuLieuDatabaseVaoTable();
-//		docDuLieuDatabaseVaoComboBox();
 		tableChonPhong.clearSelection();
 		tableChonPhong.setDefaultEditor(Object.class, null);
 
 		cmbChonPhong.setSelectedIndex(-1);
-//		xoaThongTinTrenTextField();
 		return pnlContentPane;
 	}
-
-//	@Override
-//	public void actionPerformed(ActionEvent e) {
-//		Object o = e.getSource();
-//		if (o.equals(btnThanhToan)) {
-//			int row = tableChonPhong.getSelectedRow();
-//			if (row != -1) {
-//				HoaDon hd = traPhong_dao.getHoaDonTheoMaPhong(tableChonPhong.getValueAt(row, 0).toString());
-//
-//				NhanVien nv = traPhong_dao.getNhanVienSuDung(FrameDangNhap.getTaiKhoan());
-//				String maNhanVien = nv.getMaNV();
-//
-//				hd.setThoigianTraPhong(thoiGianTraPhongDate);
-//
-//				double soGio = hd.getSoGioThue();
-//				double tienPhong = hd.tinhTongTienPhong();
-//				double tienDV = hd.getTienDichVu();
-//				double tongTien = hd.tinhTongTienThanhToan();
-//
-//				String maHD = hd.getMaHD();
-//
-//				if (traPhong_dao.update(maHD, maNhanVien, thoiGianTraPhongDate, soGio, hd.getGiaPhong(), tienPhong,
-//						tienDV, tongTien) == true) {
-//					JOptionPane.showMessageDialog(this, "Thanh toán thành công");
-//					if (tableChonPhong.getValueAt(row, 0).toString().trim().equals(FrameDatPhong.maPhongMoiDat)) {
-//						FrameDatPhong.maHDMoiDat = "";
-//						FrameDatPhong.maKHDatPhong = "";
-//						FrameDatPhong.maPhongMoiDat = "";
-//					}
-//					phongHat_dao.updateTrangThai(tableChonPhong.getValueAt(row, 0).toString().trim(), false);
-//					tableModelChonPhong.removeRow(row);
-//
-//					cmbChonPhong.removeAllItems();
-//					docDuLieuDatabaseVaoComboBox();
-//
-//					FrameDatPhong.cmbTimKhachHang.removeAllItems();
-//					FrameDatPhong.docDuLieuVaoCmbTimKH();
-//
-//					FrameDatPhong.xoaHetDLTablePhongTrong();
-//					FrameDatPhong.docDuLieuDatabaseVaoTablePhongTrong();
-//
-//					FrameDatPhong.xoaHetDLTablePhongDaDat();
-//					FrameDatPhong.docDuLieuDatabaseVaoTablePhongDaDat();
-//
-//					FrameDatPhong.tablePhongDaDat.clearSelection();
-//					tableChonPhong.clearSelection();
-//					xoaThongTinTrenTextField();
-//
-//				} else {
-//					JOptionPane.showMessageDialog(this, "Thanh toán không thành công");
-//				}
-//
-//			} else {
-//				JOptionPane.showMessageDialog(this, "Vui lòng chọn phòng cần trả");
-//			}
-//		} else if (o.equals(btnTimPhong)) {
-//			if (cmbChonPhong.getItemCount() > 0 && cmbChonPhong.getSelectedIndex() != -1) {
-//				String phongDuocChon = cmbChonPhong.getSelectedItem().toString();
-//				for (int i = 0; i < tableChonPhong.getRowCount(); i++) {
-//					if (tableChonPhong.getValueAt(i, 1).toString().trim().equals(phongDuocChon)) {
-//						tableChonPhong.setRowSelectionInterval(i, i);
-//						layThongTinHoaDon();
-//						break;
-//					}
-//				}
-//			} else {
-//				xoaThongTinTrenTextField();
-//				tableChonPhong.clearSelection();
-//			}
-//		} else if (o.equals(btnInHoaDon)) {
-//			int row = tableChonPhong.getSelectedRow();
-//			if (row != -1) {
-//				HoaDon hd = traPhong_dao.getHoaDonTheoMaPhong(tableChonPhong.getValueAt(row, 0).toString().trim());
-//
-//				NhanVien nv = traPhong_dao.getNhanVienSuDung(FrameDangNhap.getTaiKhoan());
-//
-//				new FrameHoaDonTinhTien(hd.getMaKH().getTenKH().trim(), nv.getTenNV().trim(), hd.getMaHD().trim(),
-//						hd.getThoigianDatPhong(), thoiGianTraPhongDate).setVisible(true);
-//
-//			} else {
-//				JOptionPane.showMessageDialog(this, "Vui lòng chọn phòng cần in hóa đơn");
-//			}
-//
-//		}
-//
-//	}
-//
-//	public static void docDuLieuDatabaseVaoTable() {
-//		DecimalFormat df = new DecimalFormat("#,##0.0");
-//		List<HoaDon> list = datPhong_DAO.getTatCaPhongHatDaDat();
-//		for (HoaDon hd : list) {
-//			tableModelChonPhong.addRow(new Object[] { hd.getMaPhong().getMaPhong().trim(),
-//					hd.getMaPhong().getTenPhong().trim(), df.format(hd.getMaPhong().getGiaPhong()),
-//					hd.getMaPhong().getLoaiPhong().trim(), hd.getThoigianDatPhong(), hd.getMaKH().getTenKH().trim() });
-//		}
-//	}
-//
-//	public static void docDuLieuDatabaseVaoComboBox() {
-//		List<PhongHat> list = phongHat_dao.getPhongTheoTrangThai(true);
-//		for (PhongHat phongHat : list) {
-//			cmbChonPhong.addItem(phongHat.getTenPhong().trim());
-//		}
-//	}
-//
-//	public static void xoaHetDLTableChonPhong() {
-//		DefaultTableModel dm = (DefaultTableModel) tableChonPhong.getModel();
-//		dm.setRowCount(0);
-//	}
-//
-//	public void xoaThongTinTrenTextField() {
-//		txtNhanVien.setText("");
-//		txtKH.setText("");
-//		txtTenPhong.setText("");
-//		txtLoaiPhong.setText("");
-//		txtGiaPhong.setText("");
-//		txtThoiGianDen.setText("");
-//		txtThoiGianTra.setText("");
-//		txtSoGio.setText("");
-//		txtTienPhong.setText("");
-//		txtTienDV.setText("");
-//		txtThanhToan.setText("");
-//		cmbChonPhong.setSelectedIndex(-1);
-//	}
-//
-//	@Override
-//	public void mouseClicked(MouseEvent e) {
-//		layThongTinHoaDon();
-//	}
-//
-//	@Override
-//	public void mouseEntered(MouseEvent e) {
-//	}
-//
-//	@Override
-//	public void mouseExited(MouseEvent e) {
-//	}
-//
-//	@Override
-//	public void mousePressed(MouseEvent e) {
-//	}
-//
-//	@Override
-//	public void mouseReleased(MouseEvent e) {
-//	}
-//
-//	public void layThongTinHoaDon() {
-//		int row = tableChonPhong.getSelectedRow();
-//		txtTenPhong.setText(tableChonPhong.getValueAt(row, 1).toString());
-//		txtLoaiPhong.setText(tableChonPhong.getValueAt(row, 3).toString());
-//
-//		HoaDon hd = traPhong_dao.getHoaDonTheoMaPhong(tableChonPhong.getValueAt(row, 0).toString());
-//		txtKH.setText(hd.getMaKH().getTenKH());
-//		NhanVien nv = traPhong_dao.getNhanVienSuDung(FrameDangNhap.getTaiKhoan());
-//		txtNhanVien.setText(nv.getTenNV());
-//
-//		DecimalFormat df = new DecimalFormat("#,##0.0");
-//		txtGiaPhong.setText(df.format(hd.getGiaPhong()));
-//
-//		SimpleDateFormat dt = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
-//		thoiGianTraPhongDate = new Date();
-//		String thoiGianTraPhong = dt.format(thoiGianTraPhongDate);
-//		hd.setThoigianTraPhong(thoiGianTraPhongDate);
-//
-//		txtThoiGianDen.setText(dt.format(hd.getThoigianDatPhong()));
-//		txtThoiGianTra.setText(thoiGianTraPhong);
-//		txtSoGio.setText(df.format(hd.tinhTongSoGioThue()));
-//		txtTienPhong.setText(df.format(hd.tinhTongTienPhong()));
-//		txtTienDV.setText(df.format(hd.tinhTongTienDichVu()));
-//		txtThanhToan.setText(df.format(hd.tinhTongTienDichVu() + hd.tinhTongSoGioThue() * hd.getGiaPhong()) + " VNĐ");
-//	}
 }
