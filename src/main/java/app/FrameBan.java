80c42b4549b78d64df151e952362c15c6f280054
BuiPhucNguyen <pnguyen.93.012@gmail.com>
2022-11-22 21:29:31 +0700

    fix

--- src/main/java/app/FrameBan.java
+++ src/main/java/app/FrameBan.java
@@ -421,7 +421,7 @@ public class FrameBan extends JFrame  {
 						}
 						
 						if(list.size() == 0) {
-							JOptionPane.showMessageDialog(null, "Không có phòng hát nào phù hợp với tiêu chí", "Lỗi",
+							JOptionPane.showMessageDialog(null, "Không có bàn nào phù hợp với tiêu chí", "Lỗi",
 									JOptionPane.ERROR_MESSAGE);
 							return;
 						}
